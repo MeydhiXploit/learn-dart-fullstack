2f5bec3d71c8cbf43d15c6d6e644a72f33cdc46c
MeydhiXploit <meydhimeydhi@gmail.com>
2024-09-23 14:40:23 +0800

    key late

---
+++ bin/kataKunciLate.dart
@@ -0,0 +1,10 @@
+void main() {
+  late var value = getValue();
+  print("variabel sudah di buat");
+  print(value);
+}
+
+String getValue() {
+  print('getValue() di ambil');
+  return 'Meydhi Ari Nugroho';
+}
